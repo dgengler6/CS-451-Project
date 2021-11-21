452bc9b8e8e5a18da9c8030ae66814db7ca8c0c9
dgengler6 <gengler.damien@gmail.com>
2021-11-21 19:04:53 +0100

    Add trials wit nbMessgaes

--- template_java/src/main/java/cs451/Main.java
+++ template_java/src/main/java/cs451/Main.java
@@ -90,7 +90,7 @@ public class Main {
 
         UniformReliableBroadcast urb = new UniformReliableBroadcast(hosts, me, null);
 
-        for(int i=1; i<=1;i++){
+        for(int i=1; i<=nbMessages;i++){
             Message m = new Message(i, myID, me.getIp(), me.getPort(), "");
             urb.broadcast(m);
         }
